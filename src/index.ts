e37d72aa2a460e8aad5bac62ba3eb058fa2e34fe
Pranjal Pokharel <pranjalpokharel007@gmail.com>
2022-08-15 09:23:41 +0545

    Configured webpack as bundler, TS support

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { app, BrowserWindow } from 'electron';
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from './constants';
 const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
   });
 
-  win.loadFile(path.join(path.dirname(__dirname), 'src/index.html'));
+  win.loadFile(path.join(path.dirname(__dirname), 'dist/index.html'));
 };
 
 app.whenReady().then(() => {
